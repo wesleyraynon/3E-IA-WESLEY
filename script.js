cc7a640636db8f476a093e7892b3a45644445476
wesleyHW <freire.wesley@escola.pr.gov.br>
2026-06-23 12:30:08 +0000

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta1 = "Pergunta 1";
+const pergunta2 = "Pergunta 2";
+
+const perguntas = ["Pergunta 1", "Pergunta 2"];
