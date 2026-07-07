d1fc920ff628183f51f87a7544494db5791e6589
wesleyHW <freire.wesley@escola.pr.gov.br>
2026-07-07 13:30:05 +0000

    ok

--- script.js
+++ script.js
@@ -6,73 +6,42 @@ const textoResultado = document.querySelector(".texto-resultado");
 //acima criação das constantes JS dos elementos HTML
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
+        enunciado: "A guitarra elétrica possui diversas partes fundamentais que definem a sua afinação, timbre e tocabilidade. Qual é a função principal das tarraxas, peças localizadas no headstock (mão) do instrumento?: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Controlar a tensão das cordas, permitindo afinar o instrumento adequadamente.",
+                afirmacao: "Controlar a tensão das cordas, permitindo afinar o instrumento adequadamente. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Captar a vibração mecânica das cordas e transformá-la em sinal elétrico.",
+                afirmacao: " Essa definição está incorreta para as tarraxas. A peça responsável por captar o som e gerar o sinal elétrico é o captador."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: " O posicionamento correto da mão e dos dedos no braço da guitarra é crucial para evitar lesões e garantir fluidez. Segundo grandes mestres, qual deve ser o papel e a posição ideal do polegar da mão esquerda (para destros)?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "icar totalmente escondido e apoiado na parte de trás do braço, servindo de apoio para os outros dedos."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Excelente resposta! Manter o polegar relaxado e apoiado atrás do braço garante mais agilidade e evita tensões musculares."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
+        enunciado: "Na teoria musical aplicada à guitarra, o campo harmônico é um conceito fundamental para a criação de melodias e acompanhamentos. O que é exatamente o Campo Harmônico?: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Uma linha horizontal de notas tocadas sucessivamente, representando uma única melodia.
+                afirmacao: "Essa é a definição de escala. O campo harmônico, por outro lado, é uma estrutura vertical onde as notas são tocadas juntas para formar acordes."
             },
             {
                 texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            } //fechamento do objeto do texto e afirmação da lista de alternativas
-        ] //fechamento da lista de alternativas
-    }, //fechamento do objeto com enunciado e lista de alternativas da lista de perguntas
-]; //fechamento da lista de perguntas
-
-
+    }
+    
+    
 let atual = 0; //variavel do inicio da lista de perguntas
 let perguntaAtual; //variavel correspondente a pergunta atual selecionada
 let historiaFinal = ""; //variavel que guarda os textos das afirmações para formar a frase final da IA
